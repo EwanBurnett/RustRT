1af9b93c4135393bcb842ea27332a6f06e20666d
Ewan Burnett <EwanBurnettSK@outlook.com>
2025-03-14 15:20:16 +0000

    Added RTIOW Library + Render Function

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+pub mod vec3;
+
+use image::ImageBuffer; 
+
+pub fn render(img_buf: &mut ImageBuffer<image::Rgb<u8>, Vec<u8>>){
+    let width: u32 = img_buf.width(); 
+    let height: u32 = img_buf.height(); 
+
+    //Render the image
+    for(x, y, pixel) in img_buf.enumerate_pixels_mut()
+    {
+        let r: f32 = (x as f32) / (width as f32 - 1.0); 
+        let g: f32 = (y as f32) / (height as f32 - 1.0); 
+        let r: f32 = 0.0; 
+
+        *pixel = image::Rgc([
+            (r * 255.999) as u8, 
+            (g * 255.999) as u8, 
+            (b * 255.999) as u8 
+        ]);
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-pub mod vec3;
 
 fn main() {
     println!("Hello, world!");
+
+    let image_width = 256; 
+    let image_height = 256; 
+
+    let mut image_buffer: image::ImageBuffer<image::Rgb<u8>, Vec<u8>> = image::ImageBuffer::new(image_width, image_height); 
+
+    rtiow::render(&mut image_buffer); 
+
+    image_buffer.save("image.png").unwrap(); 
 }
