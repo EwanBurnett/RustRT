b6c82f798178bd7b787ece0797a543865a0a1c9b
Ewan Burnett <EwanBurnettSK@outlook.com>
2025-03-14 16:01:12 +0000

    Vec3 length_squared and dot

--- src/vec3.rs
+++ src/vec3.rs
@@ -13,6 +13,29 @@ impl Vec3{
     pub fn new(_x: f32, _y: f32, _z: f32) -> Vec3 {
         Vec3{x: _x ,y: _y, z: _z}   //Construct a new Vector3 instance. 
     }
+
+
+    pub fn length_squared(v : &Vec3) -> f32{
+        return (v.x * v.x) + (v.y * v.y) + (v.z * v.z);
+    }
+
+    /*
+    pub fn length(v: &Vec3) -> f32{
+        return sqrt(Vec3::length_squared(&v));
+    }
+   */
+
+    pub fn dot(a: &Vec3, b: &Vec3) -> f32{
+        return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+    }
+
+    pub fn cross(u: &Vec3, v: &Vec3) {  //-> Vec3
+        /*
+        x : u.y * v.z - u.z * v.y, 
+        y : u.x * v.z - u.z * v.x,
+        z : u.x * v.y - u.y * v.z,
+        */
+    }
 }
 
 
@@ -20,6 +43,7 @@ impl Vec3{
 #[cfg(test)]
 mod test{
     use super::*;   //Access the parent's scope. 
+
     #[test]
     fn new_vec3_construction_f32_f32_f32(){
         //Configure test data
@@ -32,4 +56,17 @@ mod test{
         assert_eq!(v.y, b); 
         assert_eq!(v.z, c); 
     }
+
+    #[test]
+    fn vec3_length_squared(){
+        assert_eq!(true, true);
+    }
+
+
+    #[test]
+    fn vec3_dot_vec3(){
+        assert_eq!(true, true);
+    }
+    
+
 }
